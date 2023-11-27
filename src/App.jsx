a9d90fbbf28491d8f6a87aa965b440beb138f785
Vasileios1988 <v.milias1@gmail.com>
2023-11-27 10:09:06 +0000

    add functions to add and delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,44 @@ import "./style.css"
 
 export default function App() {
    const [newItem, setNewItem] = useState("")
+    const [todos, setTodos] = useState([])
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        setTodos((currentTodos) => {
+            return [
+                ...currentTodos,
+                 {id: crypto.randomUUID(), title: newItem, completed: false},
+            ]
+        })
+
+        setNewItem("")
+    }
+
+    function toggleTodo(id, completed){
+        setTodos(currentTodos => {
+            return currentTodos.map(todo => {
+                if(todo.id === id){
+                    return {...todo, completed}
+                }
+
+                return todo
+            })
+        })
+    }
+
+    function deleteTodo(id){
+        setTodos(currentTodos => {
+            return currentTodos.filter(todo => todo.id !== id)
+        })
+    }
+
+
 
  return (
     <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
     <div className="fomr-row">
         <label htmlfor="item">New Item</label>
         <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
@@ -14,28 +48,21 @@ export default function App() {
     <button className="btn">Add</button>
  </form>
  <h1 className="header">Todo List</h1>
- <ul>
-    <li>
+ <ul className="list">
+    {(todos.length ===  0) && "No Todos"}
+    {todos.map(todo => {
+        return <li key={todo.id}>
         <label>
-            <input type="checkbox"/>
-            Item 1
+            <input type="checkbox" checked={todo.completed}
+            onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+            {todo.title}
         </label>
-        <button className="btn btn-danger">Delete</button>
+        <button 
+        onClick={() => deleteTodo(todo.id)}
+        className="btn btn-danger">Delete</button>
     </li>
-    <li>
-        <label>
-            <input type="checkbox"/>
-            Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-    </li>
-    <li>
-        <label>
-            <input type="checkbox"/>
-            Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-    </li>
+    }
+)}
  </ul>
  </>
     
